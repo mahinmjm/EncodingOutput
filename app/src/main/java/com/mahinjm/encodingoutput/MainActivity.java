32c9644f6df3cda432f5ee0a12ba49d51894ce87
mahinmjm <mahinmjm@gmail.com>
2017-01-28 22:16:15 +0600

    first commit

---
+++ app/src/main/java/com/mahinjm/encodingoutput/MainActivity.java
@@ -0,0 +1,37 @@
+package com.mahinjm.encodingoutput;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    EditText editText;
+
+    String text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = editText.getText().toString();
+                textView.setText(Uri.encode(text));
+            }
+        });
+
+
+    }
+}
